a5326ec9d6f8d74c0d7b9af948e0cf2ff947e8ba
Oumar <osm.cre.dev@gmail.com>
2022-12-10 17:30:06 -0800

    maj console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(JournalCommandes::class)->everyMinute();
+        $schedule->command(JournalCommandes::class)->hourly();
         // $schedule->command('inspire')->hourly();
 
       /*  $schedule
